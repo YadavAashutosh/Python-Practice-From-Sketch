d0d629a8aa070cab169bfa0f8305ff3ede265329
Ashu <142735342+YadavAashutosh@users.noreply.github.com>
2026-04-10 14:37:57 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,7 +1,7 @@
 '''Write a Python program that takes the prices of 3 food items and the total number of
  friends as inputs. It must calculate the total food bill, add an 18% GST tax, 
  how much each friend needs to pay, and finally check if the total bill is greater than ₹1500
- to determine premium discount eligibility'''
+ to determine premium discount eligibility.'''
 
 food1= int(input("Enter the price of first food: " ))
 food2= int(input("Enter the price of second food: " ))
